45e328a327122d8da53bd0f1cda488b83b34c64f
Oganes <og.marckaryan@yandex.ru>
2025-11-25 19:21:37 +0300

    глава 15. Создание планировщика React.Анимация

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -1,32 +1,46 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
+import autoAnimate from "@formkit/auto-animate";
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
 
-class TodoItems extends Component{
-    constructor(props){
-        super(props)
+    this.listRef = createRef();
 
-        this.createTasks = this.createTasks.bind(this)
-        this.delete = this.delete.bind(this)
+    this.createTasks = this.createTasks.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.listRef.current) {
+      autoAnimate(this.listRef.current, {
+        duration: 250,
+        easing: "ease-out",
+      });
     }
+  }
 
-    delete(key){
-        this.props.delete(key)
-    }
+  delete(key) {
+    this.props.delete(key);
+  }
 
-    createTasks(item){
-        return <li onClick={()=>this.delete(item.key)} key={item.key}>{item.text}</li>
-    }
+  createTasks(item) {
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
+  }
 
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
 
-    render(){
-        var todoEntries = this.props.entries; 
-        var listItems = todoEntries.map(this.createTasks)
-
-        return (
-            <ul className="theList">
-                {listItems}
-            </ul>
-        )
-    }
+    return (
+      <ul className="theList" ref={this.listRef}>
+        {listItems}
+      </ul>
+    );
+  }
 }
 
-export default TodoItems
+export default TodoItems;
