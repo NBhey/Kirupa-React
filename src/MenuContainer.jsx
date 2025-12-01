adbfbcdbbe4b0faf543df9725df86f015a3c0e6e
Oganes <og.marckaryan@yandex.ru>
2025-12-01 18:20:58 +0300

    глава 16. Создание плавающего меню в React. Начало работы

---
+++ src/MenuContainer.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+class MenuContainer extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <p>Найдешь пункт, который здесь лишний?</p>
+          <ul>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li> Dolor </li>
+            <li>Sit</li>
+            <li>Bumblebees</li>
+            <li>Aenean</li>
+            <li>Consectetur</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MenuContainer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import TodoList from "./TodoList";
+import MenuContainer from "./MenuContainer";
 
 var destination = document.querySelector("#container");
 
 ReactDOM.createRoot(destination).render(
   <div>
-    <TodoList />
+    <MenuContainer />
   </div>
 );
